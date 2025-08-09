d79567a0c2e738d85fe31dcbdc4e85cbaa5f4e37
Rodrigues <7jsdrcj@gmail.com>
2025-08-09 14:25:25 -0300

    Utilizando o FOR, melhora_no_codigo.js

--- parametro.js
+++ parametro.js
@@ -1,26 +1,26 @@
                     // parametro
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+// function tocaSom (idElementoAudio) {
+//     document.querySelector(idElementoAudio).play();
+// }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+// const listaDeTeclas = document.querySelectorAll('.tecla');
 
-contador = 0;
-                    // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
-while (contador < listaDeTeclas.length) {
+// contador = 0;
+//                     // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
+// while (contador < listaDeTeclas.length) {
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
 
-    const idAudio = `#som_${instrumento}`;
+//     const idAudio = `#som_${instrumento}`;
     
-    console.log(idAudio);
+//     console.log(idAudio);
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
 
-    contador = contador + 1;
+//     contador = contador + 1;
 
-    console.log(contador);
-}
+//     console.log(contador);
+// }
