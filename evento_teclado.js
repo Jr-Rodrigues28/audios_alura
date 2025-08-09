36078490603333cf65b555f3beaca2ac8ea55e5d
Rodrigues <7jsdrcj@gmail.com>
2025-08-09 18:33:13 -0300

    Melhorando o código lhe dando confiabilidade

--- evento_teclado.js
+++ evento_teclado.js
@@ -1,40 +1,42 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+// function tocaSom (idElementoAudio) {
+//     document.querySelector(idElementoAudio).play();
+// }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+// const listaDeTeclas = document.querySelectorAll('.tecla');
 
-                                  // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
-for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+//                                   // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
+// for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`;
     
-    console.log(idAudio);
+//     console.log(idAudio);
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
 
-    console.log(contador);
+//     console.log(contador);
 
 
-    // add a classe ativa do css, para quando clicar nos botões com alguma tecla
-    // do teclado neste caso ENTER ou BARRA DE ESPAÇO, deixa da cor vermelha 
-    tecla.onkeydown = function (evento) {
+//     // add a classe ativa do css, para quando clicar nos botões com alguma tecla
+//     // do teclado neste caso ENTER ou BARRA DE ESPAÇO, deixa da cor vermelha 
+//     tecla.onkeydown = function (evento) {
 
-        console.log(evento.code == 'Space' ) 
+//         console.log(evento.code == 'Space' ) 
 
-        if (evento.code === 'Space') {
-            tecla.classList.add('ativa');
-        } 
-    }
 
-    // Caso mantenha a tecla ENTER ou ESPAÇO pressionada ela fica da cor vermelha, caso
-    // solte ela volta para a cor de origem
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+//                                     // operador lógico OR
+//         if (evento.code === 'Space' || evento.code === 'Enter') {
+//             tecla.classList.add('ativa');
+//         }
+//     }
 
-}
+//     // Caso mantenha a tecla ENTER ou ESPAÇO pressionada ela fica da cor vermelha, caso
+//     // solte ela volta para a cor de origem
+//     tecla.onkeyup = function () {
+//         tecla.classList.remove('ativa');
+//     }
+
+// }
---
+++ maisCondicoes_e_melhorandoOcodigo.js
@@ -0,0 +1,97 @@
+// function tocaSom (idElementoAudio) {
+//     document.querySelector(idElementoAudio).play();
+// }
+
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//                                   // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
+// for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`;
+    
+//     console.log(idAudio);
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
+
+//     console.log(contador);
+
+
+//     // add a classe ativa do css, para quando clicar nos botões com alguma tecla
+//     // do teclado neste caso ENTER ou BARRA DE ESPAÇO, deixa da cor vermelha 
+//     tecla.onkeydown = function (evento) {
+
+//         console.log(evento.code == 'Space' ) 
+
+
+//                                     // operador lógico OR
+//         if (evento.code === 'Space' || evento.code === 'Enter') {
+//             tecla.classList.add('ativa');
+//         }
+//     }
+
+//     // Caso mantenha a tecla ENTER ou ESPAÇO pressionada ela fica da cor vermelha, caso
+//     // solte ela volta para a cor de origem
+//     tecla.onkeyup = function () {
+//         tecla.classList.remove('ativa');
+//     }
+
+// }
+
+
+
+// esse trecho da function garante mais ainda a confiabilidade do código
+// caso alguém queira usar de forma inapropriada
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+                                  // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    console.log(contador);
+
+
+    // add a classe ativa do css, para quando clicar nos botões com alguma tecla
+    // do teclado neste caso ENTER ou BARRA DE ESPAÇO, deixa da cor vermelha 
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space' ) 
+
+
+                                    // operador lógico OR
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    // Caso mantenha a tecla ENTER ou ESPAÇO pressionada ela fica da cor vermelha, caso
+    // solte ela volta para a cor de origem
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
