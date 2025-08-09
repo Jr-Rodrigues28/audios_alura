1449ba796fae3fe2d306c950d8c01cfef73ff8aa
Rodrigues <7jsdrcj@gmail.com>
2025-08-09 15:48:51 -0300

    Utilizando onkeydow .add e onkeyup .remove

--- melhora_no_codigo.js
+++ melhora_no_codigo.js
@@ -1,24 +1,24 @@
 // Com FOR
 
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+// function tocaSom (idElementoAudio) {
+//     document.querySelector(idElementoAudio).play();
+// }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+// const listaDeTeclas = document.querySelectorAll('.tecla');
 
-                                  // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
-for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+//                                   // Aqui vale 9 que são a quantidade de audios sendo que começa com 0
+// for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`;
     
-    console.log(idAudio);
+//     console.log(idAudio);
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
 
-    console.log(contador);
+//     console.log(contador);
 
-}
+// }
